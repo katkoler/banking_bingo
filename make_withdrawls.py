2b9f69edcb183b5b0f5b7b38ac7ca80595153219
Kirsty <kirsty.elliott.93@gmail.com>
2018-10-27 22:46:03 +0100

    make_withdrawls.py

---
+++ make_withdrawls.py
@@ -0,0 +1,49 @@
+import os
+import requests
+import json
+import config
+
+apiKey = os.environ["capitalone_api_key"]
+
+accountId = os.environ["accountId"]
+
+
+url_trans = 'http://api.reimaginebanking.com/accounts/{}/withdrawals?key={}'.format(accountId,apiKey)
+
+
+withdrawl = {
+  "medium": "balance",
+  "transaction_date": "2018-10-27",
+  "status": "pending",
+  "amount": 1995,
+  "description": "string"
+}
+
+# Create a new transaction
+response = requests.post( 
+  url_trans, 
+  data=json.dumps(withdrawl),
+  headers={'content-type':'application/json'},
+  )
+
+
+print(response)
+
+if response.status_code == 201:
+  print('withdrawl made')
+
+
+
+# Output
+# [
+#     {
+#         "_id": "5bd4db6cf0cec56abfa4410d",
+#         "medium": "balance",
+#         "transaction_date": "2018-10-27",
+#         "status": "pending",
+#         "amount": 1995,
+#         "description": "string",
+#         "type": "withdrawal",
+#         "payer_id": "5bd48ec6f0cec56abfa440e9"
+#     }
+# ]
